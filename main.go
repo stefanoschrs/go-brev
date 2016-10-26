1e512440d3937fde6d52df8e46f1aac56fff3e66
Stefanos Chrs <stefanoschrs@hotmail.com>
2016-10-26 23:24:14 +0300

    Add grid in fe

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
 
 type Event struct {
+	Timestamp    int32
 	Message      string `json:"msg"`
 	URL          string `json:"url"`
 	LineNumber   string `json:"lineNo"`
@@ -31,18 +33,14 @@ func CreateEventEndpoint(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	event.Timestamp = int32(time.Now().Unix())
 	events = append(events, event)
 	json.NewEncoder(w).Encode(event)
 }
 
 func main() {
-	events = append(events, Event{
-		Message:      "test msg",
-		URL:          "test url",
-		LineNumber:   "test lineNo",
-		ColumnNumber: "test columnNo",
-		ErrorObject:  "test error",
-	})
+	events = append(events, Event{Timestamp: int32(time.Now().Unix()), Message: "test msg", URL: "test url", LineNumber: "test lineNo", ColumnNumber: "test columnNo", ErrorObject: "test error"})
+	events = append(events, Event{Timestamp: int32(time.Now().Unix()), Message: "test 1 msg", URL: "test 1 url", LineNumber: "test 1 lineNo", ColumnNumber: "test 1 columnNo", ErrorObject: "test 1 error"})
 
 	router := mux.NewRouter()
 
